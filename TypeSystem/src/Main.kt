f702718a443a566e3a11883630aed9aa33366fe7
kingkashta <mutwakil.m.osman@gmail.com>
2020-01-28 19:15:02 +0100

    Type system

---
+++ TypeSystem/src/Main.kt
@@ -0,0 +1,12 @@
+val name = "Mutwakil"
+var greeting: String? = null
+fun main() {
+    println(greeting)
+    println(name)
+
+    greeting = null
+    println(greeting)
+    greeting = "Bonjour"
+    println(greeting)
+
+}
