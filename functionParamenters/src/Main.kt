0f642a93c8d7179e2cead38f2d1fe5baadbe82be
kingkashta <mutwakil.m.osman@gmail.com>
2020-01-29 19:06:07 +0100

    functions and parameters

---
+++ functionParamenters/src/Main.kt
@@ -0,0 +1,7 @@
+
+fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
+fun main() {
+    sayHello("Hey", "Kotlin")
+    sayHello("Hello", "World")
+    sayHello("Merci", "Android")
+}
