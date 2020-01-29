f58f471208f404a3c76495898f23acdae2855f7c
kingkashta <mutwakil.m.osman@gmail.com>
2020-01-29 19:15:46 +0100

    array

---
+++ Array/src/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
+    println(interestingThings.size)
+    println(interestingThings[0])
+    println(interestingThings.get(0))
+    println(interestingThings[2])
+}
